70d01a6abcde91ad2f346b88c9158fbc44595315
Alper Celik <alper celik@RT5762ML-GGB.local>
2022-01-26 10:24:23 -0500

    dont check for absolute path

--- R/BLAST.R
+++ R/BLAST.R
@@ -16,7 +16,6 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-b
 
 last  <- function(db = NULL, type = "blastn") {
   if(is.null(db)) stop("No BLAST database specified!")
